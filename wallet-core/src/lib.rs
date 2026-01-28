a33db16826e65cf2338c0e846c846bc4bbfdb036
scrumier <sonam.crumiere@icloud.com>
2026-01-28 13:38:55 +0100

    Feat:✨ Loading screen with animation

--- wallet-core/src/lib.rs
+++ wallet-core/src/lib.rs
@@ -1,11 +1,72 @@
 #![no_std]
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Circle, Primitive, PrimitiveStyle};
 use embedded_graphics::geometry::Point;
+use embedded_graphics::mono_font::{ascii::FONT_6X10, MonoTextStyle};
+use embedded_graphics::text::Alignment::Center;
+use embedded_graphics::text::Text;
 
 
-pub fn draw_ui<D>(display: &mut D)
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum AppState {
+	Init,
+	SetupNewWallet,
+	EnterPin,
+	Settings,
+	PublicKeyGeneration,
+	SendMoney,
+	ReceiveMoney,
+}
+
+pub struct ColdWallet {
+	pub state: AppState,
+}
+
+impl ColdWallet {
+	pub fn new() -> Self {
+		Self {
+			state: AppState::Init,
+		}
+	}
+	
+	pub fn get_state(&self) -> AppState {
+		self.state.clone()
+	}
+}
+
+
+pub fn init_screen<D>(display: &mut D, animation_number: i32)
+	-> Result<(), D::Error>
+	where
+		D: DrawTarget<Color=Rgb565>,
+{
+	let font = &FONT_6X10;
+	let style = MonoTextStyle::new(font, Rgb565::WHITE);
+	let position = Point::new(120, 120);
+	
+	match animation_number % 4 {
+		0 => { Text::with_alignment("Loading |", position, style, Center).draw(display)?; }
+		1 => { Text::with_alignment("Loading /", position, style, Center).draw(display)?; }
+		2 => { Text::with_alignment("Loading -", position, style, Center).draw(display)?; }
+		3 => { Text::with_alignment("Loading \\", position, style, Center).draw(display)?; }
+		_ => { error_screen(display)?; }
+	}
+	
+	
+	Ok(())
+}
+
+pub fn error_screen<D>(display: &mut D)
+	-> Result<(), D::Error>
+	where
+		D: DrawTarget<Color=Rgb565>,
+{
+	display.clear(Rgb565::RED)?;
+	
+	Ok(())
+}
+
+pub fn draw_ui<D>(display: &mut D, state: AppState, animation_number: i32)
 	-> Result<(), D::Error>
 	where
 		D: DrawTarget<Color=Rgb565>,
@@ -13,11 +74,11 @@ pub fn draw_ui<D>(display: &mut D)
 	// Init black screen
 	display.clear(Rgb565::BLACK)?;
 	
-	// Draw red circle
-	let style = PrimitiveStyle::with_fill(Rgb565::RED);
-	Circle::with_center(Point::new(120, 120), 50)
-		.into_styled(style)
-		.draw(display)?;
+	match state {
+		AppState::Init => init_screen(display, animation_number)?,
+		_ => error_screen(display)?,
+	}
+	
 	
 	Ok(())
 }
--- wallet-sim/src/main.rs
+++ wallet-sim/src/main.rs
@@ -1,6 +1,8 @@
+use embedded_graphics::draw_target::DrawTarget;
 use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
 use embedded_graphics_simulator::{SimulatorDisplay, Window, OutputSettingsBuilder};
-use wallet_core::draw_ui;
+use wallet_core::{draw_ui, ColdWallet};
 use embedded_graphics::geometry::Size;
 
 
@@ -11,21 +13,38 @@ fn main()
 	let screen_size = Size::new(240, 240);
 	let mut display = SimulatorDisplay::<Rgb565>::new(screen_size);
 	
+	// Init wallet
+	let wallet = ColdWallet::new();
 	
-	draw_ui(&mut display)?;
+	let mut tick_counter = 0;
+	let mut animation_number = 0;
+	let mut update = false;
 	
+	display.clear(Rgb565::BLACK)?;
 	
 	// Settings for MacOS
-	let output_settings = OutputSettingsBuilder::new().scale(2).build();
+	let output_settings = OutputSettingsBuilder::new().scale(2)
+	                                                  .build();
 	let mut window = Window::new("Cold wallet", &output_settings);
+	
 	
 	// Window loop
 	'running: loop {
 		window.update(&display);
-		if window.events().any(|e| e == embedded_graphics_simulator::SimulatorEvent::Quit) {
+		if update == true {
+			draw_ui(&mut display, wallet.state, animation_number)?;
+			animation_number += 1;
+		}
+		if window.events()
+		         .any(|e| e == embedded_graphics_simulator::SimulatorEvent::Quit) {
 			break 'running;
 		}
 		std::thread::sleep(std::time::Duration::from_millis(1000));
+		
+		if tick_counter % 100 == 1 {
+			update = true;
+		}
+		tick_counter += 1;
 	}
 	
 	
